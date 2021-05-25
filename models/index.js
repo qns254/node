f75e4bd9a3e77b8449d18aa4fbac11e1b01671d5
qns254 <qnsdlrh78@naver.com>
2021-05-25 12:32:55 +0900

    라우터 구성 , 기본 템플릿 작업

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
-const sequelize = new sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 const db = {};
 
 
