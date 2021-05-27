79de67db643497a918067f80da432f2b8ecc3406
qns254 <qnsdlrh78@naver.com>
2021-05-27 16:11:52 +0900

    이력서 db처리

--- models/resume.js
+++ models/resume.js
@@ -14,13 +14,17 @@ const resume = {
 		console.log(params);
 		try {
 			// basicinfo 처리  S 
-			/**
-			 basicInfo
-  취업우대,병역
-  beneift 
-  handicapLevel: '',
-  military: '',
-			*/
+			
+			/** 취업우대 • 병역 선택 체크 S */
+			if (!params.items || params.items.indexOf("병역") == -1) {  // 취업우대,병역 항목 선택이 없으면 값 비우기
+				params.military = params.handicapLevel = params.benefit = "";	
+			}
+			/** 취업우대 • 병역 선택 체크 E */
+			
+			if (params.benefit instanceof Array) {
+				params.benefit = params.benefit.join("||");
+			}
+			
 			let sql = `UPDATE basicinfo 
 									SET 
 										  resumeTitle = :resumeTitle, 
@@ -37,7 +41,8 @@ const resume = {
 										  workKeyword = :workKeyword,
 										  benefit = :benefit,
 										  handicapLevel = :handicapLevel,
-										  military = :military`;
+										  military = :military,
+										  negotiableSalary = :negotiableSalary`;
 				let replacements = {
 					resumeTitle : params.resumeTitle,
 					name : params.name,
@@ -54,14 +59,56 @@ const resume = {
 					benefit : params.benefit || "",
 					handicapLevel : params.handicapLevel,
 					military : params.military,
+					negotiableSalary : params.negotiableSalary || 0,
 				};
 				
 				let result = await sequelize.query(sql, {
 					replacements, 
 					type : QueryTypes.INSERT,
 				});
-				console.log(result);
 			// basicinfo 처리 E 
+			// school 학력 처리 S 
+			sql = 'TRUNCATE school';
+			await sequelize.query(sql, {
+				type : QueryTypes.DELETE,
+			});
+			
+			if (params.items && params.items.indexOf('학력') != -1 && params.schoolType) {
+				if (!(params.schoolType instanceof Array)) {
+					params.schoolType = [params.schoolType];
+					params.schoolName = [params.schoolName];
+				}
+				
+				params.schoolType.forEach(async (type, index) => {
+						name = params.schoolName[index];
+						
+						const sql = "INSERT INTO school (type, name) VALUES (?, ?)";
+						await sequelize.query(sql, {
+							replacements : [type, name],
+							type : QueryTypes.INSERT,
+						});
+				});
+			}
+			// school 학력 처리 E 
+				// 경력처리
+				sql = 'TRUNCATE jobhistory';
+				await sequelize.query(sql, { type : QueryTypes.DELETE });
+				if (params.items && params.items.indexOf("경력") != -1) {
+				  if(!(params.jhCompany instanceof Array)) {
+					  params.jhCompany = [params.jhCompany];
+					  params.jhDept = [params.jhDept];
+					  params.jhStartDate =[params.jhStartDate];
+					  params.jhEndDate =[params.jhEndDate];
+					  params.jhInOffice =[params.jhInOffice];
+				      params.jhPosition =[params.jhPosition];
+					  params.jhTask =[params.jhTask];
+					  params.jhSalary =[params.jhSalary];
+					  params.jhWork =[params.jhWork];
+				  }
+				console.log(params);
+				}
+				
+				//경력처리끝
 			
 			return true;
 		} catch (err) {
